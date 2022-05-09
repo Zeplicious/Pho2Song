46548dfde6fa60f967e25767d2caadd8ddb0fd90
Daniele Maida <maida.1890742@studenti.uniroma1.it>
2022-05-09 18:03:16 +0200

    Modifica stile Landing Page

--- public/scripts/landing_page_scripts.js
+++ public/scripts/landing_page_scripts.js
@@ -26,3 +26,9 @@ backToTopButton.addEventListener("click", backToTop);
 function backToTop() {
     window.scrollTo(0, 0)
 }
+
+/* Popovers della Landing Page */
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
