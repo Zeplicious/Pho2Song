0a316b8d4dbbce8906624354559065c4c1b7bbaf
Daniele Maida <maida.1890742@studenti.uniroma1.it>
2022-05-16 15:25:40 +0200

    Bug Fix tasti di input

--- public/scripts/input_scripts.js
+++ public/scripts/input_scripts.js
@@ -24,11 +24,6 @@ upload.addEventListener("change", function () {
         document.getElementById("submit-files").disabled = true
     }
 
-    /* console.log("ueu")
-    console.log(this.files)
-    console.log(this.files[0]) 
-    console.log(URL.createObjectURL(this.files[0]))
-    console.log(this.files.fileUpload) */
     for (indice = 0; indice < this.files.length; indice++){
         tempArrayFile[indice] = this.files[indice];
 
@@ -43,16 +38,16 @@ upload.addEventListener("change", function () {
         reader.readAsText(i)
     })
 
-    //document.getElementById("sezione-input").innerHTML += "<input type='file' class='form-control' id='upload' name='fileUpload' accept='image/png, image/jpeg' multiple>"
 });
 
 function addImage() {
-    var imgText = urlForm.querySelector("#urlText");
+    let imgText = urlForm.querySelector("#urlText");
     display(imgText.value)
 };
 
 function display(res) {
     let resName = res.substring(res.lastIndexOf("/") + 1, res.length);
+
     arrayUrl.forEach(url => {
         if (res == url) {
             urlForm.innerHTML += "<div class='alert alert-danger alert-dismissible' role='alert'> Questo Url è già stato inserito. <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>"
@@ -62,22 +57,31 @@ function display(res) {
         /*urlForm.querySelector("#url").value += " "
         urlForm.querySelector("#url").value += url*/
     })
+
     if (resName != "" && urlIsNew) {
-        for (index = 1; index <= urlIndex; index++) {
-            if(document.getElementById("urlFile" + index) == null || document.getElementById("urlFile" + index).value == null){
-                previewUrlContainer.innerHTML += "<li class='list-group-item' id='urlItem" + urlIndex + "'> <input type='text' name='urls' id='urlFile" + urlIndex + "' value=" + res + " style='display: none'> <div class='row mx-auto my-2'> <div class='col-lg-8 col-xl-8 col-xxl-8 mx-auto my-2'> <p class='my-2' value= " + index + ">" + resName + "</p> </div> <div class='col-lg-4 col-xl-4 col-xxl-4 mx-auto my-2'> <button class='btn-danger btn' onclick='UrlDelete(" + index + ")'>Elimina</button> </div> </li>"
-                arrayUrl.push(res)
-                urlIsEmpty = true;
-                break;
-            }
-            else{
-                urlIsEmpty = false;
-            }
-        }
-        if(!urlIsEmpty){
-            previewUrlContainer.innerHTML += "<li class='list-group-item' id='urlItem" + urlIndex + "'> <input type='text'  name='urls' id='urlFile" + urlIndex + "' value=" + res + " style='display: none'> <div class='row mx-auto my-2'> <div class='col-lg-8 col-xl-8 col-xxl-8 mx-auto my-2'> <p class='my-2' value= " + urlIndex + ">" + resName + "</p> </div> <div class='col-lg-4 col-xl-4 col-xxl-4 mx-auto my-2'> <button class='btn-danger btn' onclick='UrlDelete(" + urlIndex + ")'>Elimina</button> </div> </li>"
+        if (urlIndex == 1) {
+            previewUrlContainer.innerHTML += "<li class='list-group-item' id='urlItem" + urlIndex + "'> <input type='text'  name='urls' id='urlFile" + urlIndex + "' value=" + res + " style='display: none'> <div class='row mx-auto my-2'> <div class='col-lg-8 col-xl-8 col-xxl-8 mx-auto my-2'> <p class='my-2' value= " + urlIndex + ">" + resName + "</p> </div> <div class='col-lg-4 col-xl-4 col-xxl-4 mx-auto my-2'> <button class='btn btn-danger' onclick='UrlDelete(" + urlIndex + ")'>Elimina</button> </div> </li>"
             urlIndex++;
             arrayUrl.push(res)
+        }
+        else {
+            for (index = 1; index < urlIndex; index++) {
+                if(document.getElementById("urlFile" + index) == null || document.getElementById("urlFile" + index).value == null) {
+                    document.getElementById("urlFile" + index).innerHTML += "<li class='list-group-item' id='urlItem" + index + "'> <input type='text' name='urls' id='urlFile" + index + "' value=" + res + " style='display: none'> <div class='row mx-auto my-2'> <div class='col-lg-8 col-xl-8 col-xxl-8 mx-auto my-2'> <p class='my-2' value= " + index + ">" + resName + "</p> </div> <div class='col-lg-4 col-xl-4 col-xxl-4 mx-auto my-2'> <button class='btn btn-danger' onclick='UrlDelete(" + index + ")'>Elimina</button> </div> </li>"
+                    arrayUrl.push(res)
+                    urlIsEmpty = true;
+                    break;
+                }
+                else {
+                    urlIsEmpty = false;
+                }
+            }
+    
+            if(!urlIsEmpty) {
+                previewUrlContainer.innerHTML += "<li class='list-group-item' id='urlItem" + urlIndex + "'> <input type='text'  name='urls' id='urlFile" + urlIndex + "' value=" + res + " style='display: none'> <div class='row mx-auto my-2'> <div class='col-lg-8 col-xl-8 col-xxl-8 mx-auto my-2'> <p class='my-2' value= " + urlIndex + ">" + resName + "</p> </div> <div class='col-lg-4 col-xl-4 col-xxl-4 mx-auto my-2'> <button class='btn btn-danger' onclick='UrlDelete(" + urlIndex + ")'>Elimina</button> </div> </li>"
+                urlIndex++;
+                arrayUrl.push(res)
+            }
         }
     }
     else if (resName == "") {
@@ -97,10 +101,3 @@ function UrlDelete(index){
     document.getElementById("urlItem" + index).style.display = "none";
     console.log(document.getElementById("urlFile" + index).value);
 }
-
-/* Prova nuovo upload di files */
-
-/*function injectFileInputTag() {
-    console.log("Sono nello script")
-    document.getElementById("sezione-input").innerHTML += "<input type='file' name='userPhoto' multiple onchange='injectFileInputTag()'/>"
-}*/
