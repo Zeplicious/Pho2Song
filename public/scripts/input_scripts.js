f0f9f083ce85ae8e9bc93e05db48879d2e4da6fd
Daniele Maida <maida.1890742@studenti.uniroma1.it>
2022-05-17 16:56:01 +0200

    Aggiustamento funzione di input dei files

--- public/scripts/input_scripts.js
+++ public/scripts/input_scripts.js
@@ -1,5 +1,6 @@
-const upload = document.getElementById("upload");
-const previewFileContainer = document.getElementById("filePreview")
+const uploadFiles = document.getElementById("upload-files");
+const filesList = document.getElementById("files-list")
+const fileInputArea = document.getElementById("file-input-area")
 
 const urlPreview = document.getElementById("urlPreview")
 const urlForm = document.getElementById("urlForm")
@@ -7,7 +8,7 @@ const urlInputArea = document.getElementById("urlInputArea")
 const urlSubmitButton= document.getElementById("submit-urls")
 
 var arrayUrl = [];
-var arrayFile = [];
+var arrayFiles = [];
 var tempArrayFile = [];
 var urlIsEmpty = false;
 //defining HTML alert
@@ -17,12 +18,61 @@ const alertInvalidHTML = "<div class='alert alert-danger alert-dismissible' role
 //initializing global variables for urls
 var urlListItemId = 0;
 
+//initializing global variables for urls
+var fileInputId = 0;
+
 var fileIndex = 1;
 var fileIsNew = true;
 var m = 0
 
+/************* Files management section *************/
 
-upload.addEventListener("change", function () {
+//Utiliy functions for files
+
+function setUpForFiles(){
+    arrayFiles = [];
+    fileInputId = 0;
+    fileInputArea.innerHTML += '<input id="file-input' + fileinputId + '" class="form-control" type="file" name="images" accept="image/*" multiple onchange="addFiles()">'
+}
+
+function isNewForFiles(arrayOfInputFiles){                  //This function return true if all files in input are new, false otherwise
+    for (let i = 0; i < arrayOfInputFiles.length; i++) {
+        for (let j = 0; j < arrayFiles.length; j++) {
+            if (arrayOfInputFiles[i] == arrayFiles[j]) {
+                return false
+            }
+            
+        }
+    }
+    return true
+}
+
+function invalidateInputFiles(target){
+    target.parentElement.removeChild(target);
+    fileInputArea.innerHTML += '<input id="file-input' + (fileInputId + 1) + '" class="form-control" type="file" name="images" accept="image/*" multiple onchange="addFiles()">'
+}
+
+function reloadInputText(target){
+    target.style.display='none'//disabilito l'input relativo al i-esimo click su 'Agggiungi'
+    //creo l'input relativo al i+1-esimo click su 'Agggiungi'
+    urlInputArea.innerHTML += "<input type='text'  class='form-control' name='urls' id='urlSent" + (urlListItemId+1) + "' 'placeholder='https://...''>" 
+}
+
+function addFiles() {
+    let fileInput = document.getElementById("file-input" + fileInputId)
+    if(!isNew(fileInput.files)){ 
+        //document.getElementById('accordionSection').innerHTML += alertAlreadyInHTML;
+        invalidateInputFiles(fileInput)
+    }
+    else{
+        reloadInputText(fileInput)
+        display(fileInput.files)
+    }
+
+    urlListItemId++
+}
+
+/* uploadFiles.addEventListener("change", function () {
 
     console.log(this.files)
 
@@ -41,26 +91,30 @@ upload.addEventListener("change", function () {
     tempArrayFile.forEach(i => {
         const reader = new FileReader();
             reader.addEventListener("load", function () {
-                previewFileContainer.innerHTML += "<li class='list-group-item' id='fileItem" + fileIndex + "'> <div class='row mx-auto my-2 align-items-center'> <div class='col-lg-8 col-xl-8 col-xxl-8 mx-auto my-2'> <p class='my-auto p-file' value= " + fileIndex + ">" + i.name + "</p> </div> <div class='col-lg-4 col-xl-4 col-xxl-4 mx-auto my-2'> <button class='btn-danger btn' onclick='FileDelete(" + fileIndex + ")'>Elimina</button> </div> </li>"
+                fileList.innerHTML += "<li class='list-group-item' id='fileItem" + fileIndex + "'> <div class='row mx-auto my-2 align-items-center'> <div class='col-lg-8 col-xl-8 col-xxl-8 mx-auto my-2'> <p class='my-auto p-file' value= " + fileIndex + ">" + i.name + "</p> </div> <div class='col-lg-4 col-xl-4 col-xxl-4 mx-auto my-2'> <button class='btn-danger btn' onclick='FileDelete(" + fileIndex + ")'>Elimina</button> </div> </li>"
                 fileIndex += 1
             })
         reader.readAsText(i)
     })
 
-});
+}); */
 
 function FileDelete(index) {
     document.getElementById("fileItem" + index).value = null;
     document.getElementById("fileItem" + index).style.display = "none"; 
     console.log(document.getElementById("fileItem" + index).value)
 }
- 
+
+/************* URLs management section *************/
+
+//Setup the used variables
 function setUp(){
     arrayUrl = [];
     urlListItemId = 0;
     urlInputArea.innerHTML+= "<input type='text'  class='form-control' name='urls' id='urlSent" + urlListItemId+ "'placeholder='https://...' >"
 }
-//URL Checking
+
+//URL validation
 function isValidHttpUrl(string) {
     let url;
     
@@ -72,18 +126,22 @@ function isValidHttpUrl(string) {
   
     return url.protocol === "http:" || url.protocol === "https:";
 }
+
+//URL checking if it is already in arrayUrl
 function isNew(url){
     for (let index = 0; index < arrayUrl.length; index++) {
         if (url == arrayUrl[index])return false
     }
     return true
 }
+
 //URL manipulating input texts
 function reloadInputText(target){
     target.style.display='none'//disabilito l'input relativo al i-esimo click su 'Agggiungi'
     //creo l'input relativo al i+1-esimo click su 'Agggiungi'
     urlInputArea.innerHTML += "<input type='text'  class='form-control' name='urls' id='urlSent" + (urlListItemId+1) + "' 'placeholder='https://...''>" 
 }
+
 function invalidateInputText(target){
     target.parentElement.removeChild(target);
     urlInputArea.innerHTML += "<input type='text'  class='form-control' name='urls' id='urlSent" + (urlListItemId+1) + "' 'placeholder='https://...''>" 
